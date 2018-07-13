f07bb351111927278e5ac415be8f18dd40749e7a
Juanets <jdgs.gt@gmail.com>
2018-07-12 19:05:20 -0700

    Expose dummy API url

--- ghostwriter/urls.py
+++ ghostwriter/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from stories.views import upper
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('upper/', upper),
 ]
