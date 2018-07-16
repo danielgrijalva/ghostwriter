587078588b589ff9348de4ee32a499b4651703f1
Daniel Grijalva <jdgs.gt@gmail.com>
2018-07-16 10:06:53 -0700

    Moved twitter config to settings.py

--- ghostwriter/settings.py
+++ ghostwriter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Twitter keys
+
+CONSUMER_KEY = config('CONSUMER_KEY')
+CONSUMER_SECRET = config('CONSUMER_SECRET')
+ACCESS_TOKEN = config('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = config('ACCESS_TOKEN_SECRET')
+LIST_OWNER = config('LIST_OWNER')
+LIST_NAME = config('LIST_NAME')
