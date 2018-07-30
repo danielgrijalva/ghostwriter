ce3f3f17c1130b5d9aa7bdb7536fbc49cff4286c
Daniel Grijalva <jdgs.gt@gmail.com>
2018-07-30 15:01:06 -0700

    Refactor textgenrnn to work with ghostwriter
    - Deleted a bunch of unused stuff
    - Renamed some things
    - Made "interactive" mode default

--- brain/textgenrnn.py
+++ brain/textgenrnn.py
@@ -2,78 +2,56 @@ from keras.preprocessing.text import Tokenizer
 import numpy as np
 import json
 from pkg_resources import resource_filename
-from .utils import *
-from .model import textgenrnn_model
+from brain.utils import *
+from brain.model import textgenrnn_model
 import re
 
 
 class textgenrnn:
     META_TOKEN = '<s>'
-    config = {
-        'rnn_layers': 2,
-        'rnn_size': 128,
-        'rnn_bidirectional': False,
-        'max_length': 40,
-        'max_words': 10000,
-        'dim_embeddings': 100,
-        'word_level': False,
-        'single_text': False
-    }
-    default_config = config.copy()
+    config = {}
 
     def __init__(self, weights_path=None,
                  vocab_path=None,
-                 config_path=None,
-                 name="textgenrnn"):
+                 config_path=None):
 
         if weights_path is None:
             weights_path = resource_filename(__name__,
-                                             'textgenrnn_weights.hdf5')
+                                             'data/weights.hdf5')
 
         if vocab_path is None:
             vocab_path = resource_filename(__name__,
-                                           'textgenrnn_vocab.json')
+                                           'data/vocab.json')
 
-        if config_path is not None:
-            with open(config_path, 'r',
-                      encoding='utf8', errors='ignore') as json_file:
-                self.config = json.load(json_file)
-
-        self.config.update({'name': name})
-        self.default_config.update({'name': name})
+        if config_path is None:
+            config_path = resource_filename(__name__,
+                                           'data/config.json')
 
         with open(vocab_path, 'r',
                   encoding='utf8', errors='ignore') as json_file:
             self.vocab = json.load(json_file)
 
-        self.tokenizer = Tokenizer(filters='', char_level=True)
+        with open(config_path, 'r',
+                    encoding='utf8', errors='ignore') as json_file:
+            self.config = json.load(json_file)
+
+        self.indices_char = dict((self.vocab[c], c) for c in self.vocab)
+        self.tokenizer = Tokenizer(filters='', char_level=False)
         self.tokenizer.word_index = self.vocab
         self.num_classes = len(self.vocab) + 1
         self.model = textgenrnn_model(self.num_classes,
                                       cfg=self.config,
                                       weights_path=weights_path)
-        self.indices_char = dict((self.vocab[c], c) for c in self.vocab)
 
-    def generate(self, n=1, return_as_list=False, prefix=None,
-                 temperature=0.5, max_gen_length=300, interactive=False,
-                 top_n=3):
-        gen_texts = []
-        for _ in range(n):
-            gen_text = textgenrnn_generate(self.model,
-                                           self.vocab,
-                                           self.indices_char,
-                                           prefix,
-                                           temperature,
-                                           self.config['max_length'],
-                                           self.META_TOKEN,
-                                           self.config['word_level'],
-                                           self.config.get(
-                                               'single_text', False),
-                                           max_gen_length,
-                                           interactive,
-                                           top_n)
-            if not return_as_list:
-                print("{}\n".format(gen_text))
-            gen_texts.append(gen_text)
-        if return_as_list:
-            return gen_texts
+
+    def generate(self, temperature, max_gen_length, top_n):
+        gen_text = textgenrnn_generate(self.model,
+                                        self.vocab,
+                                        self.indices_char,
+                                        temperature,
+                                        self.config['max_length'],
+                                        self.META_TOKEN,
+                                        max_gen_length,
+                                        top_n)
+
+        return gen_text
--- brain/utils.py
+++ brain/utils.py
@@ -5,129 +5,51 @@ import numpy as np
 import re
 
 
-def textgenrnn_sample(preds, temperature, interactive=False, top_n=3):
+def textgenrnn_sample(preds, temperature, top_n=3):
     '''
-    Samples predicted probabilities of the next character to allow
-    for the network to show "creativity."
+    Samples predicted probabilities of the next word.
     '''
 
     preds = np.asarray(preds).astype('float64')
-
-    if temperature is None or temperature == 0.0:
-        return np.argmax(preds)
-
     preds = np.log(preds + K.epsilon()) / temperature
     exp_preds = np.exp(preds)
     preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
     
-    if not interactive:
-        index = np.argmax(probas)
-
-        # prevent function from being able to choose 0 (placeholder)
-        # choose 2nd best index from preds
-        if index == 0:
-            index = np.argsort(preds)[-2]        
-    else:
-        # return list of top N chars/words
-        # descending order, based on probability
-        index = (-preds).argsort()[:top_n]
+    index = (-preds).argsort()[:top_n]
 
     return index
 
 
-def textgenrnn_generate(model, vocab,
-                        indices_char, prefix=None, temperature=0.5,
-                        maxlen=40, meta_token='<s>',
-                        word_level=False,
-                        single_text=False,
-                        max_gen_length=300,
-                        interactive=False,
-                        top_n=3):
+def textgenrnn_generate(model, vocab, indices_char, temperature,
+                        maxlen, meta_token,max_gen_length, top_n):
     '''
     Generates and returns a single text.
     '''
 
-    collapse_char = ' ' if word_level else ''
-
-    if single_text:
-        text = list(prefix) if prefix else ['']
-        max_gen_length += maxlen
-    else:
-        text = [meta_token] + list(prefix) if prefix else [meta_token]
-    next_char = ''
-
-    if not isinstance(temperature, list):
-        temperature = [temperature]
+    text = [meta_token] + [meta_token]
+    next_word = ''
 
     if model_input_count(model) > 1:
         model = Model(inputs=model.input[0], outputs=model.output[1])
 
-    while next_char != meta_token and len(text) < max_gen_length:
-        encoded_text = textgenrnn_encode_sequence(text[-maxlen:],
-                                                vocab, maxlen)
-        next_temperature = temperature[(len(text) - 1) % len(temperature)]
+    while next_word != meta_token and len(text) < max_gen_length:
+        encoded_text = textgenrnn_encode_sequence(
+            text[-maxlen:],
+            vocab,
+            maxlen
+        )
 
-        if not interactive:
-            # auto-generate text without user intervention
-            next_index = textgenrnn_sample(
-                model.predict(encoded_text, batch_size=1)[0],
-                next_temperature)
-            next_char = indices_char[next_index]
-            text += [next_char]                    
-        else:
-            # ask user what the next char/word should be
-            options_index = textgenrnn_sample(
-                model.predict(encoded_text, batch_size=1)[0],
-                next_temperature,
-                interactive=interactive,
-                top_n=top_n
-            )
-            options = [indices_char[idx] for idx in options_index]
-            print('Controls:\n\ts: stop.\tx: backspace.\to: write your own.')
-            print('\nOptions:')
+        # get indexes of top n options
+        options_index = textgenrnn_sample(
+            model.predict(encoded_text, batch_size=1)[0],
+            temperature,
+            top_n=top_n
+        )
 
-            for i, option in enumerate(options, 1):
-                print('\t{}: {}'.format(i, option))
+        # get top n options from vocab using indexes
+        options = [indices_char[idx] for idx in options_index]
 
-            print('\nProgress: {}'.format(collapse_char.join(text)[3:]))
-            print('\nYour choice?')
-            user_input = input('> ')
-
-            try:
-                user_input = int(user_input)
-                next_char = options[user_input-1]
-                text += [next_char]
-            except ValueError:
-                if user_input == 's':
-                    next_char = '<s>'
-                    text += [next_char]
-                elif user_input == 'o':
-                    other = input('> ')
-                    text += [other]                    
-                elif user_input == 'x':
-                    try:
-                        del text[-1]
-                    except IndexError:
-                        pass
-                else:
-                    print('That\'s not an option!')
-
-    # if single text, ignore sequences generated w/ padding
-    # if not single text, strip the <s> meta_tokens
-    if single_text:
-        text = text[maxlen:]
-    else:
-        text = text[1:-1]
-
-    text_joined = collapse_char.join(text)
-
-    # If word level, remove spaces around punctuation for cleanliness.
-    if word_level:
-        punct = '\\n\\t'
-        text_joined = re.sub(" ([{}]) ".format(punct), r'\1', text_joined)
-
-    return text_joined
+        return options
 
 
 def textgenrnn_encode_sequence(text, vocab, maxlen):
